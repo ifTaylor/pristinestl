c26fec0582a88a3c8f776071c3a25e03c16b287a
Taylor Turner <tturner@bastiansolutions.com>
2023-10-10 10:34:25 -0500

    about kim

--- components/about_kim.tsx
+++ components/about_kim.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 
 function AboutKim({}) {
   return (
     <Container>
-        <img src="/p_logo.png" alt="Pristine STL Logo" style={{ maxHeight: '150px' }}></img>
-        <h3>Make this the about me.</h3>
+      <Row>
+        <Col className="flex-center container-padding" md={6}>
+          <Image
+            src="/kim_headshot.png"
+            alt="Owner's Image"
+            fluid
+            style={{ maxHeight: '400px', borderRadius: '80px' }}
+          />
+        </Col>
+        <Col md={6}>
+`         <div style={{ verticalAlign: 'bottom' }}>
+            <img src="/p_logo.png" alt="Pristine STL Logo" style={{ maxHeight: '150px', display: 'inline' }} />
+            <h2 style={{ display: 'inline', verticalAlign: 'bottom' }}>About Me</h2>
+          </div>
+          <p className='item-padding'>
+            Hi, I'm Kim Chettle, the proud owner of Pristine STL LLC. I've been running this cleaning business since 2020, and it has been an incredible journey. As a hardworking mom, I understand the importance of maintaining a clean and healthy home environment for your family.
+          </p>
+          <p className='item-padding'>
+            Our mission at Pristine STL LLC is to provide exceptional cleaning services that delight our clients. My team and I take pride in our work, and our dedicated team ensures that every corner of your home shines.
+          </p>
+          <p className='item-padding'>
+            I know the challenges of balancing work and family life, so let us take care of your cleaning needs while you focus on what matters most to you.
+          </p>
+        </Col>
+      </Row>
     </Container>
   );
 }
