03280697babace00c0760194e7cf5565ab829025
Taylor Turner <tturner@bastiansolutions.com>
2023-10-17 08:49:19 -0500

    add dependencies and use translate

--- components/service_chooser.tsx
+++ components/service_chooser.tsx
@@ -1,29 +1,45 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import { useSpring, animated } from 'react-spring';
+import { useInView } from 'react-intersection-observer';
 
 function ServiceChooser({
     setResidentialQuote,
     setCommercialQuote
 }) {
+  const [ref, inView] = useInView({
+    triggerOnce: false,
+  });
+
+  const colAnimation = useSpring({
+    opacity: inView ? 1 : 0,
+    transform: inView ? 'translateY(0)' : 'translateY(200px)',
+  });
 
   return (
-    <Container className="text-center mt-5" >
-    <h2>Choose your service to get started today!</h2>
-    <Row className="justify-content-center">
-      <Col md={4}>
-        <a className="service" onClick={() => setResidentialQuote(true)}>
-          <h3>Residential Cleaning</h3>
-          <div>We provide comprehensive residential cleaning services to keep your home spotless.</div>
-        </a>
-      </Col>
-      <Col md={4}>
-        <a className="service" onClick={() => setCommercialQuote(true)}>
-        <h3>Commercial Cleaning</h3>
-            <div>Our expert team ensures a clean and sanitized work environment for your business.</div>
-        </a>
-      </Col>
-    </Row>
-  </Container>
+    <Container className="text-center mt-5">
+      <h2>Choose your service to get started today!</h2>
+      <div ref={ref}>
+        <Row className="justify-content-center">
+          <Col md={4}>
+            <animated.div style={colAnimation}>
+              <a className="service" onClick={() => setResidentialQuote(true)}>
+                <h3>Residential Cleaning</h3>
+                <div>We provide comprehensive residential cleaning services to keep your home spotless.</div>
+              </a>
+            </animated.div>
+          </Col>
+          <Col md={4}>
+            <animated.div style={colAnimation}>
+              <a className="service" onClick={() => setCommercialQuote(true)}>
+                <h3>Commercial Cleaning</h3>
+                <div>Our expert team ensures a clean and sanitized work environment for your business.</div>
+              </a>
+            </animated.div>
+          </Col>
+        </Row>
+      </div>
+    </Container>
   );
 }
 
