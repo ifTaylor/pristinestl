a787f954a73151742a7ead9afcb87dbf1e53805f
Taylor Turner <tturner@bastiansolutions.com>
2023-11-21 17:08:31 -0600

    van bar

--- pages/header.tsx
+++ pages/header.tsx
@@ -21,53 +21,42 @@ export default function Header(
           throw new Error('Function not implemented.');
       } }/>
       <Navbar expand="lg" className="navbar-light">
-        <Container>
-          <Row>
-            <Col>
-              <Container className="flex-center">
-                <Navbar.Toggle type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                  <span className="navbar-toggler-icon"></span>
-                </Navbar.Toggle>
-                <Navbar.Brand href={Pages.HomePage}>
-                  <img src="/pristine_logo.png" alt="Pristine STL Logo" style={{ maxWidth: '250px', maxHeight: '150px' }} />
-                </Navbar.Brand>
-              </Container>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Container>
-                <div className="flex-center">
-                  <div className="header-phone-number">
-                      <a className="nav-link" href="tel:+13147751571">(314) 775-1571</a>
-                  </div>
-                  <div>
-                    <a className="cta-button" onClick={() => setResidentialQuote(true)}>Get a Quote</a>
-                  </div>
-                </div>
-              </Container>
-            </Col>
-          </Row>
-          <Row className="mt-1">
-            <Col>
-              <Navbar.Collapse id="navbarNav">
-                <Nav className="ml-auto">
-                  {Object.values(Pages).map((page) => {
-                    return (
-                      <Nav.Link
-                        key={page}
-                        href={page}
-                        active={currentPage === page}
-                        className={currentPage === page ? "underlined" : ""}
-                      >
-                        {PageNames[page]}
-                      </Nav.Link>
-                    )
-                  })}
-                </Nav>
-              </Navbar.Collapse>
-            </Col>
-          </Row>
+        <Container style={{ justifyContent: 'start' }}>
+          <Navbar.Toggle
+              aria-controls="navbarNav"
+            />
+            <Navbar.Brand href={Pages.HomePage}style={{ justifyContent: 'center' }}>
+              <img src="/pristine_logo.png" alt="Pristine STL Logo" style={{ maxWidth: '250px', maxHeight: '150px' }} />
+            </Navbar.Brand>
+        </Container>
+        <Container className="mt-2" style={{ display: 'flex', justifyContent: 'center' }}>
+          <div className="flex-center">
+            <div className="header-phone-number">
+                <a className="nav-link" href="tel:+13147751571">(314) 775-1571</a>
+            </div>
+            <div>
+              <a className="cta-button" onClick={() => setResidentialQuote(true)}>Get a Quote</a>
+            </div>
+          </div>
+        </Container>
+        <Container className="mt-2">
+          <Navbar.Collapse id="navbarNav">
+            <Nav className="ml-auto" style={{ justifyContent: 'center', display: 'flex', flexDirection: 'row' }}>
+              {Object.values(Pages).map((page) => {
+                return (
+                  <Nav.Link
+                    key={page}
+                    href={page}
+                    active={currentPage === page}
+                    className={currentPage === page ? "underlined" : ""}
+                    style={{ marginRight: '10px' }}
+                  >
+                    {PageNames[page]}
+                  </Nav.Link>
+                )
+              })}
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </Container>
